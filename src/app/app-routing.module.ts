ee97fd7b3d1183426c2883eb6ca4161b2eecb99a
Isabelli <isabellisilva506@gmail.com>
2022-11-24 09:01:33 -0300

    formsApp
    login/registro/cadastro: aula 24/11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,11 @@ const routes: Routes = [
   {
     path: 'registro',
     loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
   }
+
 ];
 @NgModule({
   imports: [
